67efd2d2d57cd2beebcb244bea12581683f05d59
Mazdak <Mazdak.software@gmail.com>
2022-10-13 15:03:07 +0330

    Create fal.py

---
+++ fal.py
@@ -0,0 +1,22 @@
+import random
+
+from userbot import catub
+
+plugin_category = "fun"
+
+
+@catub.cat_cmd(
+    pattern="fal$",
+    command=("fal", plugin_category),
+    info={
+        "header": "get a fall",
+        "description": "get a fall",
+        "usage": "{tr}fal",
+        "examples": "{tr}fal",
+    },
+)
+async def fal(event):
+    fal_number = random.randrange(1, 150)
+    url = f"http://api.codebazan.ir/fal/hafez/{fal_number}.gif"
+
+    await event.client.send_file(event.chat_id, url, caption="بیا اینم فالت داداش 🔥")
